3e6a2bcb8368d5868ee65039f47a52f243545849
Toung <spexial110@gmail.com>
2025-01-26 23:03:54 +0800

    登入功能完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+console.clear();
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0)
+
+    const baseApi = import.meta.env.VITE_BASE_URL;
+    const apiPath = import.meta.env.VITE_API_PATH;
+
+    const [account, setAccount] = useState({
+        "username": "example@test.com",
+        "password": "example"
+    });
+    const [resErrMessage, setResErrMessage] = useState("");
+
+
+    const handleInputChange = (e) => {
+        const { value, name } = e.target; // 解構的方式去找出對應的值
+        setAccount({
+            ...account, // 要把原先資料抓進來，不然會每次都只其中一個的資料
+            [name]: value
+            // [name]: 等同於 username: 或 password:
+        });
+    }
+
+    const handleSingIn = async (e) => {
+        e.preventDefault(); // 可用此方式將預設行為取消掉，讓使用者可以直接按enter就可進入，不限制只透過按鈕點選
+        try {
+            const res = await axios.post(`${baseApi}/v2/admin/signin`, account);
+        }
+        catch (error) {
+            setResErrMessage(error.response?.data?.message);
+        }
+    };
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>測試</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+            <h2 className="mb-4">請先登入</h2>
+            <form onSubmit={handleSingIn}>
+                {/* 通常form會把送出時的資料放在form裡面，而不是用一按鈕去做觸發 */}
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail2">電子郵件</label>
+                    <input
+                        name="username"
+                        value={account.username}
+                        type="email"
+                        className="form-control"
+                        id="exampleInputEmail2"
+                        placeholder="請輸入信箱"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="form-group my-3">
+                    <label htmlFor="exampleInputPassword2">密碼</label>
+                    <input
+                        name="password"
+                        value={account.password}
+                        type="password"
+                        className="form-control"
+                        id="exampleInputPassword2"
+                        placeholder="請輸入密碼"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                {resErrMessage && (<p className="text-danger" >{resErrMessage}</p>)}
+                <button className="btn btn-success" >
+                    登入
                 </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    )
-}
+            </form>
+        </div>
+    );
+};
 
-export default App
+
+export default App;
